73a52efe5863104945239d8acd4ef343ba6b64fd
Fexty12573 <bogofogo1@gmail.com>
2026-05-04 13:01:51 +0200

    Make linter run on self hosted runner

--- Assets/Scripts/Chest.cs
+++ Assets/Scripts/Chest.cs
@@ -8,6 +8,7 @@ public class Chest : MonoBehaviour, IInteractable
     {
         foreach (var item in m_drops)
         {
+            // test change to force linter to run
             ItemStackDrop.Spawn(new ItemStack(item, 1), transform.position + Vector3.right);
         }
     }
